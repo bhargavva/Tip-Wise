341b1e04593224629457c2c462bfe7bbf73512f5
Bhargav AV <venkatabhargavit@gmail.com>
2024-01-05 22:42:44 +0530

    This commit adds state to tip amount variable and adds label and a numeric keyboard.

--- app/src/main/java/com/apptechinnovationlab/tipwise/MainActivity.kt
+++ app/src/main/java/com/apptechinnovationlab/tipwise/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,9 +23,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apptechinnovationlab.tipwise.ui.theme.TipWiseTheme
@@ -63,7 +70,9 @@ fun TipWiseLayout() {
                .padding(bottom = 16.dp, top = 40.dp)
                .align(Alignment.Start)
        )
-       EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+       EditNumberField(modifier = Modifier
+           .padding(bottom = 32.dp)
+           .fillMaxWidth())
        Text(text = stringResource(id = R.string.tip_amount, "$0.00"),
            style = MaterialTheme.typography.displaySmall)
 
@@ -73,8 +82,16 @@ fun TipWiseLayout() {
 }
 
 @Composable
-private fun EditNumberField(modifier: Modifier){
-    TextField(value = "", onValueChange = {}, modifier = modifier)
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput by remember { mutableStateOf("") }
+    TextField(
+        value = amountInput,
+        label = { Text(stringResource(R.string.bill_amount)) },
+        singleLine = true,
+        onValueChange = { amountInput = it },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
 }
 
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String{
